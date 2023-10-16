6634013fbcc687ab102c489b2da41196bf6afc60
chanwoo4267 <chanwopa@student.42seoul.kr>
2023-10-16 12:36:15 +0900

    get specific frame

--- srcs/opencv/specific_frame.py
+++ srcs/opencv/specific_frame.py
@@ -3,11 +3,18 @@ import cv2
 
 cap = cv2.VideoCapture("/Users/chanwoo4267/Downloads/deepface/sample_video/1.mp4")
 
+given_time = 3 # set video to 3sec
+
 amount_of_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) # total frames of video
+fps = cap.get(cv2.CAP_PROP_FPS) # frame rate of video
 
-frame_num = 200 # frame number to extract
+current_frame = given_time * fps
 
-cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num - 1)
+if current_frame >= amount_of_frames:
+    print("Given time is out of video running time")
+    exit()
+
+cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame - 1) # must -1 because frame index starts from 0
 
 res, frame = cap.read()
 
